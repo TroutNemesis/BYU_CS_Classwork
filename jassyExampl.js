3941d4c7b3e0e97dfd5a750dda5dc19861ddfc49
TroutNemesis <matthew@linfords.com>
2016-01-13 17:19:48 -0700

    Create jassyExampl.js

---
+++ jassyExampl.js
@@ -0,0 +1,17 @@
+
+function retsTheSame(thing)
+{
+	return thing;
+};
+
+var throwing = function(){
+	throw new Error();
+};
+
+function random(){
+	return Math.floor(Math.random()*100000);
+}
+
+function thisCallsSomethingElse(carpeDiem){
+	events.yolo(carpeDiem);
+}
